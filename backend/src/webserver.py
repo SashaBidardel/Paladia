ec8605a6aa8500dadde681f58f4f90aef875e077
iElectro <ejose.acc@gmail.com>
2022-02-16 15:00:37 -0500

    Mejora en la generacion de dato

--- backend/src/webserver.py
+++ backend/src/webserver.py
@@ -2,7 +2,7 @@ from flask import Flask, json, request, jsonify
 from flask_cors import CORS
 
 from src.lib.utils import object_to_json
-from src.domain.books import Book
+from src.domain.book import BookRepository, Book
 
 
 def create_app(repositories):
@@ -13,39 +13,14 @@ def create_app(repositories):
     def hello_world():
         return "...magic!"
 
-    @app.route("/api/info", methods=["GET"])
-    def info_get():
-        info = repositories["info"].get_info()
-        return object_to_json(info)
-
     @app.route("/api/books", methods=["GET"])
     def books_get():
         all_books = repositories["books"].get_books()
         return object_to_json(all_books)
 
-    @app.route("/api/books", methods=["POST"])
-    def add_book():
-        body = request.json
-        book = Book(**body)
-        repositories["books"].save(book)
-
-        return ''
-
     @app.route("/api/books/<id>", methods=["GET"])
     def book_get_by_id(id):
         book = repositories["books"].get_book_by_id(id)
         return object_to_json(book)
-
-    @app.route("/api/books/<id>", methods=["DELETE"])
-    def book_delete_by_id(id):
-        repositories["books"].delete(id)
-        return ''
-
-    @app.route("/api/books/<id>", methods=["POST"])
-    def book_edit_by_id(id):
-        body = request.json
-        book = Book(**body)
-        repositories["books"].edit(book)
-        return ''
 
     return app
