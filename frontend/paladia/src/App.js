06b69009d9178bdabc83f68d044ed20422779ba4
HernandoSilvaLeal <hernandosilvaleal@gmail.com>
2022-02-16 12:30:28 -0500

    Cambios que se convervaron de Navbar

--- frontend/paladia/src/App.js
+++ frontend/paladia/src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
 import Search from "./views/Search/Search.jsx";
-<<<<<<< HEAD
 import Navbar from './views/Navbar/Navbar.jsx'
-=======
 import Category from "./views/Home/components/Category/Category.jsx";
 import Cards from './views/Cards/Cards';
->>>>>>> e149b15b2a3bcbaaa40b02fc06b334faddd0c32c
 
 function App() {
   return (
     <div className="App">
-      <Search />
-<<<<<<< HEAD
       <Navbar/>
-=======
+      <Search />
       <Category />
       <Cards />
->>>>>>> e149b15b2a3bcbaaa40b02fc06b334faddd0c32c
     </div>
   );
 }
--- frontend/paladia/src/views/Navbar/Navbar.jsx
+++ frontend/paladia/src/views/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import style from "./Navbar.module.css";
-import { FiAlignJustify } from "react-icons/fi"
+import { FiAlignJustify } from "react-icons/fi";
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import logop from "../../assets/images/logop.png"
+
+
 
 export default function Menu(){
     return(
       <div className={style.container}>
-        <div className={style.icon}>  <FiAlignJustify/>  </div>
+        <div className={style.icon1}>  <FiAlignJustify/>  </div>
+
+        <div className={style.logo}> </div>
+
+        <div className={style.container2}>  <h1 className={style.titulo}>Paladia</h1> </div>
+        <div className={style.container3}>
+           <div className={style.icon2}>  <FaUser/>  </div>
+           <div className={style.icon3}>  <FaShoppingCart/>  </div>
+        </div>
       </div>
     );
 }
+
+
+//          <img src={logop} alt="ImagenLogo"/>
