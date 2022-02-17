8f67c8106db72d3c113cf4c24a1bb62c68c968ee
Daniel <danilog1000@gmail.com>
2022-02-17 08:04:07 -0500

    detailed book correction

--- frontend/paladia/src/views/Card/Card.jsx
+++ frontend/paladia/src/views/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classes from './Card.module.css'
-import { FaRegStar, FaTimes } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import { useState } from 'react';
 
 const Card = ( props ) => {
@@ -10,36 +10,37 @@ const Card = ( props ) => {
   return (
     <>
       <div className={`${classes.Card} ${enlarge && classes.EnlargeActive}`}>
-      <div 
-        className={`${classes.Close} ${enlarge && classes.CloseActive}`}
-        onClick={() => setEnlarge(false)}
-      >
-        <FaTimes />
-      </div>
+        <div 
+          className={`${classes.Close} ${enlarge && classes.CloseActive}`}
+          onClick={() => setEnlarge(false)}
+        >
+          <FaTimes />
+        </div>
+
         <img className={classes.Image} src="http://sobrelibros.net/wp-content/uploads/2010/06/cien_anos_de_soledad.jpg" alt="" />
+        <p className={`${classes.Description} ${enlarge && classes.DescriptionActive}`}>
+          fdsdf lkadml alkdm adsdfsdfs da sd asd  ajsd ajlsdnakldnlad alsdnlakdnkandlka sdlasdnklasndlkandla sdlknasdkl asldnasldknaldk asdlknsldkmañksd lkansdkla sdlkansd lkadnlma sdkñamsd aldnkas d
+        </p>
+
         <div className={`${classes.Info} ${enlarge && classes.InfoActive}`}>
-          <h2 className={classes.Title}>100 años de soledad</h2>
+          <h2 className={`${classes.Title} ${enlarge && classes.TitleActive}`}>100 años de soledad</h2>
           <span className={classes.Category}><strong className={classes.Subtitle}>Género:</strong>Realismo mágico</span>
           <span className={classes.Price}><strong className={classes.Subtitle}>Precio:</strong>10$</span>
-          <p
-            className={`${classes.Description} ${enlarge && classes.DescriptionActive}`}
-          >
-            <strong>Descripción:</strong>fdsdf lkadml alkdm asldkmañksd lkansdkla sdlkansd lkadnlma sdkñamsd aldnkas d
-          </p>
           <div className={classes.Buttons}>
-            <button 
+            <button
               className={`${classes.Button} ${enlarge && classes.ButtonActive}`}
               onClick={() => {setEnlarge(true)}}
             >
               Ver detalles
             </button>
-            <FaRegStar className={classes.Star} />
+            {/* <FaRegStar className={classes.Star} /> */}
           </div>
         </div>
+
       </div>
       <div className={`${enlarge && classes.Bg}`}></div>
     </>
   )
 }
 
-export default Card
+export default Card;
